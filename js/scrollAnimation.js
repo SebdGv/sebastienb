0dc56e9a27578ed06f0c6da2bf855fc5999df49c
bes dgv <bes.dgv@gmail.com>
2024-08-27 12:06:32 +0200

    js orga

---
+++ js/scrollAnimation.js
@@ -0,0 +1,68 @@
+// SCROLL ANIMATION
+import ScrollTrigger from "../../node_modules/gsap/ScrollTrigger.js";
+import gsap from "../../node_modules/gsap/index.js";
+// Enregistrez le plugin ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
+
+const cardsContainer = document.querySelector(".works-container");
+const cards = document.querySelector(".cards");
+const title = document.querySelector(".works h2");
+const aboutImg = document.querySelector(".about-img img");
+
+export function scrollAnimation() {
+  function getScrollAmount() {
+    const totalWidth = cards.scrollWidth;
+    const viewportWidth = window.innerWidth;
+    return -(totalWidth - viewportWidth + viewportWidth / 2);
+  }
+  if (window.matchMedia("(min-width: 1024px)").matches) {
+    // Créer une timeline
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: cardsContainer,
+        start: "top top",
+        end: "+=3000px",
+        scrub: 1,
+        pin: ".works",
+        invalidateOnRefresh: true,
+      },
+    });
+
+    tl.to(cards, {
+      x: getScrollAmount,
+      duration: 1,
+      ease: "none",
+    });
+    // change background color
+    tl.to(
+      cardsContainer,
+      {
+        backgroundColor: "#000",
+        duration: 0.85,
+        ease: "none",
+      },
+      "-=0.9"
+    );
+    // change title color
+    tl.to(
+      title,
+      {
+        color: "#fff",
+        duration: 1,
+        ease: "none",
+      },
+      "-=0.9"
+    );
+  }
+}
+gsap.to(aboutImg, {
+  scrollTrigger: {
+    trigger: aboutImg,
+    start: "top 80%",
+    end: "bottom 20%",
+    scrub: 1,
+  },
+  y: -100,
+  duration: 1,
+  ease: "none",
+});
--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 import { toggleMenu } from "./js/burgerMenu.js";
 import { initializeModal } from "./js/modal.js";
 import { displayProjects } from "./js/projectLoader.js";
-import gsap from "./node_modules/gsap/index.js";
-import ScrollTrigger from "./node_modules/gsap/ScrollTrigger.js";
+import { scrollAnimation } from "./js/scrollAnimation.js";
 
-// Enregistrez le plugin ScrollTrigger
-gsap.registerPlugin(ScrollTrigger);
-
-const cardsContainer = document.querySelector(".works-container");
-const cards = document.querySelector(".cards");
-const title = document.querySelector(".works h2");
 const burger = document.querySelector(".burger");
-const aboutContainer = document.querySelector(".about");
-const aboutImg = document.querySelector(".about-img");
-const aboutText = document.querySelector(".about-text");
 
 // OPEN MENU
 burger.addEventListener("click", () => {
@@ -23,50 +13,5 @@ burger.addEventListener("click", () => {
 document.addEventListener("DOMContentLoaded", async () => {
   await displayProjects();
   initializeModal();
+  scrollAnimation();
 });
-
-// SCROLL ANIMATION
-
-function getScrollAmount() {
-  const totalWidth = cards.scrollWidth;
-  const viewportWidth = window.innerWidth;
-  return -(totalWidth - viewportWidth + viewportWidth / 2);
-}
-if (window.matchMedia("(min-width: 1024px)").matches) {
-  // Créer une timeline
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: cardsContainer,
-      start: "top top",
-      end: "+=3000px",
-      scrub: 1,
-      pin: ".works",
-      invalidateOnRefresh: true,
-    },
-  });
-
-  tl.to(cards, {
-    x: getScrollAmount,
-    duration: 1,
-    ease: "none",
-  });
-
-  tl.to(
-    cardsContainer,
-    {
-      backgroundColor: "#000",
-      duration: 1,
-      ease: "none",
-    },
-    "-=0.9"
-  );
-  tl.to(
-    title,
-    {
-      color: "#fff",
-      duration: 1,
-      ease: "none",
-    },
-    "-=0.9"
-  );
-}
